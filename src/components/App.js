db2f764c9621a5c991e857a6137d24c4bfc556e2
Arun Singh Pundir <Arunsinghpundir.325@gmail.com>
2022-12-21 17:02:05 +0530

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,19 @@
 import React from "react";
 import "../styles/App.css";
 import Movie from "./Movie";
+import MovieProvider from "./MovieContext";
 import MoviesList from "./MoviesList";
 
 const App = () => {
   
   return (
+    <MovieProvider>
     <div id="main">
       <h1>Movie List</h1>
       <MoviesList />
       <Movie />
     </div>
+    </MovieProvider>
   );
 };
 
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,15 +1,18 @@
 import React from "react";
-
+import { MovieContext } from "./MovieContext";
 class Movie extends React.Component {
-  render() {
+  static contextType = MovieContext;
 
+  render() {
+    const {movies,selectedMovieId } = this.context;
     // If no movie is selected, show message - "Invalid Id".
-    
+    const movie = movies.find((movie)=>movie.id === selectedMovieId);
     return (
       <div id="movie-banner">
-        <h2>Movie title</h2>
-        <h2>Movie year</h2>
-        <h2>Movie director</h2>
+        {movie?(<><h2>{movie.title}</h2>
+        <h2>{movie.year}</h2>
+        <h2>{movie.director}</h2></>):(<h1>Invalid Id</h1>)
+        } 
       </div>
     );
   }
