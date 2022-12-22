7ad3acf643c5b6e076a442fa1c1c40cf8423ccf6
Arun Singh Pundir <101244758+Arunsinghpundir@users.noreply.github.com>
2022-12-22 22:58:37 +0530

    Update Movie.js

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,21 +1,24 @@
-import React from "react";
-import { MovieContext } from "./MovieContext";
-class Movie extends React.Component {
-  static contextType = MovieContext;
+import React, { useContext } from "react";
+import UserContext from "./MovieContext";
 
-  render() {
-    const {movies,selectedMovieId } = this.context;
-    // If no movie is selected, show message - "Invalid Id".
-    const movie = movies.find((movie)=>movie.id === selectedMovieId);
-    return (
-      <div id="movie-banner">
-        {movie?(<><h2>{movie.title}</h2>
-        <h2>{movie.year}</h2>
-        <h2>{movie.director}</h2></>):(<h1>Invalid Id</h1>)
-        } 
-      </div>
-    );
-  }
-}
+const Movie = (props) => {
+  // If no movie is selected, show message - "Invalid Id".
+  const first = useContext(UserContext);
+  const id = props.value;
+  // console.log(props.value);
+  return (
+    <div id="movie-banner">
+      {!props.condtion ? (
+        <h1>Invalid Id</h1>
+      ) : (
+        <div>
+          <h2>{first[id].title}</h2>
+          <h2>{first[id].year}</h2>
+          <h2>{first[id].director}</h2>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default Movie;
