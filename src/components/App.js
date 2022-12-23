cbc113cc0f46414308efa16b09cba41a8cba396c
Arun Singh Pundir <101244758+Arunsinghpundir@users.noreply.github.com>
2022-12-24 02:05:03 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,18 @@
-import React, { useState, useContext } from "react";
+import React from "react";
 import "../styles/App.css";
+import MovieProvider from "./MovieContext";
 import Movie from "./Movie";
-import UserContext from "./MovieContext";
 import MoviesList from "./MoviesList";
 
 const App = () => {
-  const [condtion, setCondtion] = useState(false);
-  const [st, setState] = useState(1);
-  const state = useContext(UserContext);
-
-  const handleClick = (e, key) => {
-    // console.log(key);
-    setState(key);
-    setCondtion(true);
-  };
   return (
-    <div id="main">
-      <h1>Movie List</h1>
-      <UserContext.Provider value={state}>
-        <MoviesList handleClick={handleClick} />
-        <Movie value={st - 1} condtion = {condtion}  />
-      </UserContext.Provider>
-    </div>
+    <MovieProvider>
+      <div id="main">
+        <h1>Movie List</h1>
+        <MoviesList />
+        <Movie />
+      </div>
+    </MovieProvider>
   );
 };
 
